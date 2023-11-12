787027ba5ef9af75258d4bdc186501e62ab257c2
Liz Robson <lizziearobson@gmail.com>
2023-11-12 20:12:18 +0000

    adding alternate X and O functions

--- src/App.js
+++ src/App.js
@@ -10,28 +10,36 @@ function Square({ value, onSquareClick }) {
 
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
-
-  function handleClick() {
+  const [xIsNext, setIsNext] = useState(true);
+  function handleClick(i) {
     const nextSquares = squares.slice();
-    nextSquares[0] = "X";
+    console.log(nextSquares); // creates copy of the array
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+      setSquares(nextSquares);
+      setIsNext(!xIsNext);
+    }
+    nextSquares[i] = "X"; //Assigns value of 'X' to the first element of the array
     setSquares(nextSquares);
   }
   return (
     <>
       <div className="board-row">
-        <Square value={squares[0]} onSquareClick={handleClick} />
-        <Square value={squares[1]} />
-        <Square value={squares[2]} />
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square value={squares[3]} />
-        <Square value={squares[4]} />
-        <Square value={squares[5]} />
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square value={squares[6]} />
-        <Square value={squares[7]} />
-        <Square value={squares[8]} />
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   );
